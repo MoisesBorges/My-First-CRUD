5763ef1ccba882410994579a14c4ca471dbe612d
Moises Borges <mborges@opencadd.local>
2020-01-20 14:56:36 -0300

    Add rest

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  #get 'welcome/index'
   resources :coins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'welcome#index'
+  
 end
