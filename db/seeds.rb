801b03236aa10692e93bdd2dab36ebd5ad6d6aeb
Moises Borges <moisesnascimento777@hotmail.com>
2020-01-24 16:48:10 -0300

    Creation of the seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Creating coins..."
+
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  url_image: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
+
+)
+
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETH",
+  url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/471px-Ethereum_logo_2014.svg.png"
+)
+
+Coin.create!(
+  description: "DASH",
+  acronym: "DASH",
+  url_image: "https://pngimage.net/wp-content/uploads/2018/05/dash-png-9.png"
+
+)
+
+puts "Coins successfully created!"
